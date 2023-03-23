4c69306579bb71054d45f59ff2e263078a13798b
KONE2610 <ibrahimkone96@gmail.com>
2023-03-23 12:08:03 +0000

    Deployment config for AWS

--- application.py
+++ application.py
@@ -39,4 +39,7 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True) 
+    app.run(host="0.0.0.0") 
+
+
+# On enlève debug=True pour les besoins de déploiement sur AWS
